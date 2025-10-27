8c8697e1f061ba49ba816fc34a0af957a333fb1e
Naganjali11 <naganjali425@gmail.com>
2025-10-27 15:38:11 +0530

    javafullstackprograms

---
+++ Temp.java
@@ -0,0 +1,15 @@
+package com.codegnan.map;
+
+public class Temp {
+	int i;
+	public Temp(int i) {
+		this.i=i;
+	}
+	public int hashCode() {
+		return i;
+		
+	}
+	public String toString() {
+	return i+"";
+	}
+}
